47afc16ce99b9037d886bd0b2299a8ee737b11be
Stefano-Para <stefano.zina@gmail.com>
2021-06-25 16:30:07 +0200

    ultimo ritocco di codice

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
 $drugo = "\"Walter... Walter, non posso presentare il conto al muso giallo che mi ha pisciato sul tappeto... Si può sapere di che c. stai parlando?\"";
 $walter = "\"Di che c. stai parlando tu? La questione qui non è il muso giallo, io parlo di una linea tracciata sulla sabbia Drugo, se qualcuno la oltrepassa tu non devi \r\n... E poi scusa, muso giallo non mi sembra l'appellativo corretto per un cinese, asiatico è preferibile.\"";
-
 $testo = '<pre>' . "Drugo: " . $drugo . "\r\n \r\n" . "Walter: " . $walter . '</pre>';
-
 ?>
 
 <!DOCTYPE html>
@@ -16,15 +14,12 @@ $testo = '<pre>' . "Drugo: " . $drugo . "\r\n \r\n" . "Walter: " . $walter . '</
     </head>
     <body>
     <h1>The Big Lebosky</h1>
-
     <h3>Testo</h3>
     <p>
         <?php
             echo '<pre>';
             var_dump("<-- quanto è lungo questo testo?" . $testo);
             echo '</pre>';
-
-            // echo $testo;
         ?>
     </p>
     <h3>Testo censurato</h3>
@@ -36,9 +31,6 @@ $testo = '<pre>' . "Drugo: " . $drugo . "\r\n \r\n" . "Walter: " . $walter . '</
             var_dump("<-- quanto è lungo questo testo?" . $_GET["word"] , "***", $testo);
 
             echo '</pre>';
-              
-
-            // echo str_replace($_GET["word"] , "***", $testo);
         ?>
     </p>
 
